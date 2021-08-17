ff6914d08bada14fc66191f045282f87d9f70fc2
Gustavs <gustavs.gutmanis@gmail.com>
2021-08-17 13:50:03 +0300

    fix(#56): adding native Element query arguments to Calendar GQL

--- packages/plugin/src/Bundles/GraphQL/Arguments/EventArguments.php
+++ packages/plugin/src/Bundles/GraphQL/Arguments/EventArguments.php
@@ -2,99 +2,103 @@
 
 namespace Solspace\Calendar\Bundles\GraphQL\Arguments;
 
-use craft\gql\base\Arguments;
+use craft\gql\base\ElementArguments;
 use GraphQL\Type\Definition\Type;
 
-class EventArguments extends Arguments
+class EventArguments extends ElementArguments
 {
     public static function getArguments(): array
     {
-        return [
-            'id' => [
-                'name' => 'id',
-                'type' => Type::listOf(Type::int()),
-                'description' => "Filter events by their ID's",
-            ],
-            'loadOccurrences' => [
-                'name' => 'loadOccurrences',
-                'type' => Type::boolean(),
-                'description' => 'Should occurrences be loaded',
-            ],
-            'calendarId' => [
-                'name' => 'calendarId',
-                'type' => Type::listOf(Type::int()),
-                'description' => 'Load events specific to a calendar',
-            ],
-            'authorId' => [
-                'name' => 'authorId',
-                'type' => Type::listOf(Type::int()),
-                'description' => 'Load events specific to an author',
-            ],
-            'allDay' => [
-                'name' => 'allDay',
-                'type' => Type::boolean(),
-                'description' => 'Load all-day events',
-            ],
-            'rangeStart' => [
-                'name' => 'rangeStart',
-                'type' => Type::string(),
-                'description' => 'Specify start range',
-            ],
-            'rangeEnd' => [
-                'name' => 'rangeEnd',
-                'type' => Type::string(),
-                'description' => 'Specify end range',
-            ],
-            'startDate' => [
-                'name' => 'startDate',
-                'type' => Type::string(),
-                'description' => 'Specify start date',
-            ],
-            'startsBefore' => [
-                'name' => 'startsBefore',
-                'type' => Type::string(),
-                'description' => 'Events that start before given date',
-            ],
-            'startsBeforeOrAt' => [
-                'name' => 'startsBeforeOrAt',
-                'type' => Type::string(),
-                'description' => 'Events that start before or at the given date',
-            ],
-            'startsAfter' => [
-                'name' => 'startsAfter',
-                'type' => Type::string(),
-                'description' => 'Events that start after given date',
-            ],
-            'startsAfterOrAt' => [
-                'name' => 'startsAfterOrAt',
-                'type' => Type::string(),
-                'description' => 'Events that start after or at the given date',
-            ],
-            'endsBefore' => [
-                'name' => 'endsBefore',
-                'type' => Type::string(),
-                'description' => 'Events that ends before given date',
-            ],
-            'endsBeforeOrAt' => [
-                'name' => 'endsBeforeOrAt',
-                'type' => Type::string(),
-                'description' => 'Events that ends before or at the given date',
-            ],
-            'endsAfter' => [
-                'name' => 'endsAfter',
-                'type' => Type::string(),
-                'description' => 'Events that ends after given date',
-            ],
-            'endsAfterOrAt' => [
-                'name' => 'endsAfterOrAt',
-                'type' => Type::string(),
-                'description' => 'Events that ends after or at the given date',
-            ],
-            'endDate' => [
-                'name' => 'endDate',
-                'type' => Type::string(),
-                'description' => 'Specify end date',
-            ],
-        ];
+        return array_merge(
+            parent::getArguments(),
+            self::getContentArguments(),
+            [
+                'id' => [
+                    'name' => 'id',
+                    'type' => Type::listOf(Type::int()),
+                    'description' => "Filter events by their ID's",
+                ],
+                'loadOccurrences' => [
+                    'name' => 'loadOccurrences',
+                    'type' => Type::boolean(),
+                    'description' => 'Should occurrences be loaded',
+                ],
+                'calendarId' => [
+                    'name' => 'calendarId',
+                    'type' => Type::listOf(Type::int()),
+                    'description' => 'Load events specific to a calendar',
+                ],
+                'authorId' => [
+                    'name' => 'authorId',
+                    'type' => Type::listOf(Type::int()),
+                    'description' => 'Load events specific to an author',
+                ],
+                'allDay' => [
+                    'name' => 'allDay',
+                    'type' => Type::boolean(),
+                    'description' => 'Load all-day events',
+                ],
+                'rangeStart' => [
+                    'name' => 'rangeStart',
+                    'type' => Type::string(),
+                    'description' => 'Specify start range',
+                ],
+                'rangeEnd' => [
+                    'name' => 'rangeEnd',
+                    'type' => Type::string(),
+                    'description' => 'Specify end range',
+                ],
+                'startDate' => [
+                    'name' => 'startDate',
+                    'type' => Type::string(),
+                    'description' => 'Specify start date',
+                ],
+                'startsBefore' => [
+                    'name' => 'startsBefore',
+                    'type' => Type::string(),
+                    'description' => 'Events that start before given date',
+                ],
+                'startsBeforeOrAt' => [
+                    'name' => 'startsBeforeOrAt',
+                    'type' => Type::string(),
+                    'description' => 'Events that start before or at the given date',
+                ],
+                'startsAfter' => [
+                    'name' => 'startsAfter',
+                    'type' => Type::string(),
+                    'description' => 'Events that start after given date',
+                ],
+                'startsAfterOrAt' => [
+                    'name' => 'startsAfterOrAt',
+                    'type' => Type::string(),
+                    'description' => 'Events that start after or at the given date',
+                ],
+                'endsBefore' => [
+                    'name' => 'endsBefore',
+                    'type' => Type::string(),
+                    'description' => 'Events that ends before given date',
+                ],
+                'endsBeforeOrAt' => [
+                    'name' => 'endsBeforeOrAt',
+                    'type' => Type::string(),
+                    'description' => 'Events that ends before or at the given date',
+                ],
+                'endsAfter' => [
+                    'name' => 'endsAfter',
+                    'type' => Type::string(),
+                    'description' => 'Events that ends after given date',
+                ],
+                'endsAfterOrAt' => [
+                    'name' => 'endsAfterOrAt',
+                    'type' => Type::string(),
+                    'description' => 'Events that ends after or at the given date',
+                ],
+                'endDate' => [
+                    'name' => 'endDate',
+                    'type' => Type::string(),
+                    'description' => 'Specify end date',
+                ],
+            ]
+        );
     }
 }
