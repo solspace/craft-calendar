bd715e8158b93c6f945ace610efbcfc84038f643
Gustavs <gustavs.gutmanis@gmail.com>
2021-11-30 18:17:04 +0200

    fix: skipping events table primary key migration for people that already have set ID to primary key

--- packages/plugin/src/migrations/m211001_063936_AddPrimaryKeyForEventsTable.php
+++ packages/plugin/src/migrations/m211001_063936_AddPrimaryKeyForEventsTable.php
@@ -9,6 +9,12 @@ class m211001_063936_AddPrimaryKeyForEventsTable extends Migration
     public function safeUp()
     {
         $table = $this->getDb()->getTableSchema('{{%calendar_events}}');
+
+        $idColumn = $table->getColumn('id');
+        if ($idColumn->isPrimaryKey) {
+            return true;
+        }
+
         if (!$table->getColumn('internalId')) {
             $this->addColumn('{{%calendar_events}}', 'internalId', $this->primaryKey());
         }
