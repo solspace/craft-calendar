41ada5408d1f8119a0213aa3ddef96b3bda1dc2f
Gustavs <gustavs.gutmanis@gmail.com>
2022-02-07 18:45:59 +0200

    fix(#97): passing GQL field definitions through `TypeManager` to fetch custom fields

--- packages/plugin/src/Bundles/GraphQL/Interfaces/EventInterface.php
+++ packages/plugin/src/Bundles/GraphQL/Interfaces/EventInterface.php
@@ -2,6 +2,7 @@
 
 namespace Solspace\Calendar\Bundles\GraphQL\Interfaces;
 
+use craft\gql\TypeManager;
 use craft\gql\types\DateTime;
 use GraphQL\Type\Definition\Type;
 use Solspace\Calendar\Bundles\GraphQL\Types\EventType;
@@ -31,147 +32,154 @@ class EventInterface extends AbstractInterface
 
     public static function getFieldDefinitions(): array
     {
-        return [
-            'id' => [
-                'name' => 'id',
-                'type' => Type::int(),
-                'description' => "The event's ID",
-            ],
-            'uid' => [
-                'name' => 'uid',
-                'type' => Type::string(),
-                'description' => "The event's UUID",
-            ],
-            'postDate' => [
-                'name' => 'postDate',
-                'type' => Type::string(),
-                'description' => "The event's Post Date",
-            ],
-            'calendarId' => [
-                'name' => 'calendarId',
-                'type' => Type::int(),
-                'description' => "The event's Calendar ID",
-            ],
-            'calendar' => [
-                'name' => 'calendar',
-                'type' => CalendarInterface::getType(),
-                'description' => "The event's Calendar",
-            ],
-            'title' => [
-                'name' => 'title',
-                'type' => Type::string(),
-                'description' => "The event's title",
-            ],
-            'authorId' => [
-                'name' => 'authorId',
-                'type' => Type::int(),
-                'description' => "The event's author ID",
-            ],
-            'startDate' => [
-                'name' => 'startDate',
-                'type' => DateTime::getType(),
-                'description' => "The event's start date",
-            ],
-            'startDateLocalized' => [
-                'name' => 'startDateLocalized',
-                'type' => DateTime::getType(),
-                'description' => "The event's start date localized",
-            ],
-            'initialStartDate' => [
-                'name' => 'initialStartDate',
-                'type' => DateTime::getType(),
-                'description' => "The event's initial start date",
-            ],
-            'endDate' => [
-                'name' => 'endDate',
-                'type' => DateTime::getType(),
-                'description' => "The event's start date",
-            ],
-            'endDateLocalized' => [
-                'name' => 'endDateLocalized',
-                'type' => DateTime::getType(),
-                'description' => "The event's end date localized",
-            ],
-            'initialEndDate' => [
-                'name' => 'initialEndDate',
-                'type' => DateTime::getType(),
-                'description' => "The event's initial end date",
-            ],
-            'slug' => [
-                'name' => 'slug',
-                'type' => Type::string(),
-                'description' => 'The slug of the event',
-            ],
-            'duration' => [
-                'name' => 'duration',
-                'type' => DurationInterface::getType(),
-                'description' => 'The duration of the event',
-            ],
-            'url' => [
-                'name' => 'url',
-                'type' => Type::string(),
-                'description' => 'The url of the event',
-            ],
-            'allDay' => [
-                'name' => 'allDay',
-                'type' => Type::boolean(),
-                'description' => 'Is event an all day event',
-            ],
-            'multiDay' => [
-                'name' => 'multiDay',
-                'type' => Type::boolean(),
-                'description' => 'Is event a multi-day event',
-            ],
-            'rrule' => [
-                'name' => 'rrule',
-                'type' => Type::string(),
-                'description' => "The event's RRule",
-            ],
-            'freq' => [
-                'name' => 'freq',
-                'type' => Type::string(),
-                'description' => 'Repeat frequency',
-            ],
-            'interval' => [
-                'name' => 'interval',
-                'type' => Type::int(),
-                'description' => 'Repeat interval',
-            ],
-            'count' => [
-                'name' => 'count',
-                'type' => Type::int(),
-                'description' => 'Repeat count',
-            ],
-            'until' => [
-                'name' => 'until',
-                'type' => DateTime::getType(),
-                'description' => 'Repeat until date',
-            ],
-            'untilLocalized' => [
-                'name' => 'untilLocalized',
-                'type' => DateTime::getType(),
-                'description' => 'Repeat until date localized',
-            ],
-            'byMonth' => [
-                'name' => 'byMonth',
-                'type' => Type::string(),
-                'description' => 'Repeat by months',
-            ],
-            'byYearDay' => [
-                'name' => 'byYearDay',
-                'type' => Type::string(),
-                'description' => 'Repeat by year days',
-            ],
-            'byMonthDay' => [
-                'name' => 'byMonthDay',
-                'type' => Type::string(),
-                'description' => 'Repeat by month days',
-            ],
-            'byDay' => [
-                'name' => 'byDay',
-                'type' => Type::string(),
-                'description' => 'Repeat by days',
-            ],
-        ];
+        return TypeManager::prepareFieldDefinitions(
+            array_merge(
+                parent::getFieldDefinitions(),
+                self::getConditionalFields(),
+                [
+                    'id' => [
+                        'name' => 'id',
+                        'type' => Type::int(),
+                        'description' => "The event's ID",
+                    ],
+                    'uid' => [
+                        'name' => 'uid',
+                        'type' => Type::string(),
+                        'description' => "The event's UUID",
+                    ],
+                    'postDate' => [
+                        'name' => 'postDate',
+                        'type' => Type::string(),
+                        'description' => "The event's Post Date",
+                    ],
+                    'calendarId' => [
+                        'name' => 'calendarId',
+                        'type' => Type::int(),
+                        'description' => "The event's Calendar ID",
+                    ],
+                    'calendar' => [
+                        'name' => 'calendar',
+                        'type' => CalendarInterface::getType(),
+                        'description' => "The event's Calendar",
+                    ],
+                    'title' => [
+                        'name' => 'title',
+                        'type' => Type::string(),
+                        'description' => "The event's title",
+                    ],
+                    'authorId' => [
+                        'name' => 'authorId',
+                        'type' => Type::int(),
+                        'description' => "The event's author ID",
+                    ],
+                    'startDate' => [
+                        'name' => 'startDate',
+                        'type' => DateTime::getType(),
+                        'description' => "The event's start date",
+                    ],
+                    'startDateLocalized' => [
+                        'name' => 'startDateLocalized',
+                        'type' => DateTime::getType(),
+                        'description' => "The event's start date localized",
+                    ],
+                    'initialStartDate' => [
+                        'name' => 'initialStartDate',
+                        'type' => DateTime::getType(),
+                        'description' => "The event's initial start date",
+                    ],
+                    'endDate' => [
+                        'name' => 'endDate',
+                        'type' => DateTime::getType(),
+                        'description' => "The event's start date",
+                    ],
+                    'endDateLocalized' => [
+                        'name' => 'endDateLocalized',
+                        'type' => DateTime::getType(),
+                        'description' => "The event's end date localized",
+                    ],
+                    'initialEndDate' => [
+                        'name' => 'initialEndDate',
+                        'type' => DateTime::getType(),
+                        'description' => "The event's initial end date",
+                    ],
+                    'slug' => [
+                        'name' => 'slug',
+                        'type' => Type::string(),
+                        'description' => 'The slug of the event',
+                    ],
+                    'duration' => [
+                        'name' => 'duration',
+                        'type' => DurationInterface::getType(),
+                        'description' => 'The duration of the event',
+                    ],
+                    'url' => [
+                        'name' => 'url',
+                        'type' => Type::string(),
+                        'description' => 'The url of the event',
+                    ],
+                    'allDay' => [
+                        'name' => 'allDay',
+                        'type' => Type::boolean(),
+                        'description' => 'Is event an all day event',
+                    ],
+                    'multiDay' => [
+                        'name' => 'multiDay',
+                        'type' => Type::boolean(),
+                        'description' => 'Is event a multi-day event',
+                    ],
+                    'rrule' => [
+                        'name' => 'rrule',
+                        'type' => Type::string(),
+                        'description' => "The event's RRule",
+                    ],
+                    'freq' => [
+                        'name' => 'freq',
+                        'type' => Type::string(),
+                        'description' => 'Repeat frequency',
+                    ],
+                    'interval' => [
+                        'name' => 'interval',
+                        'type' => Type::int(),
+                        'description' => 'Repeat interval',
+                    ],
+                    'count' => [
+                        'name' => 'count',
+                        'type' => Type::int(),
+                        'description' => 'Repeat count',
+                    ],
+                    'until' => [
+                        'name' => 'until',
+                        'type' => DateTime::getType(),
+                        'description' => 'Repeat until date',
+                    ],
+                    'untilLocalized' => [
+                        'name' => 'untilLocalized',
+                        'type' => DateTime::getType(),
+                        'description' => 'Repeat until date localized',
+                    ],
+                    'byMonth' => [
+                        'name' => 'byMonth',
+                        'type' => Type::string(),
+                        'description' => 'Repeat by months',
+                    ],
+                    'byYearDay' => [
+                        'name' => 'byYearDay',
+                        'type' => Type::string(),
+                        'description' => 'Repeat by year days',
+                    ],
+                    'byMonthDay' => [
+                        'name' => 'byMonthDay',
+                        'type' => Type::string(),
+                        'description' => 'Repeat by month days',
+                    ],
+                    'byDay' => [
+                        'name' => 'byDay',
+                        'type' => Type::string(),
+                        'description' => 'Repeat by days',
+                    ],
+                ]
+            ),
+            self::getName()
+        );
     }
 }
