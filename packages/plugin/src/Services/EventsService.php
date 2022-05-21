44f5886ef0b9ee5d232b7c3e58fa5e4e9d137c85
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2022-05-21 12:19:03 +0300

    fix(#132): fixed an issue where translations were being overwritten for all sites

--- packages/plugin/src/Services/EventsService.php
+++ packages/plugin/src/Services/EventsService.php
@@ -204,7 +204,7 @@ class EventsService extends Component
             $transaction = \Craft::$app->db->beginTransaction();
 
             try {
-                $isSaved = \Craft::$app->elements->saveElement($event, $validateContent);
+                $isSaved = \Craft::$app->elements->saveElement($event, $validateContent, $isNewEvent);
                 if ($isSaved) {
                     $this->reindexSearchForAllSites($event);
 
