61c2de60da8fba51074ca1c0cd99595fae027741
Sean Delaney <sean@solspace.com>
2025-07-24 09:38:53 +0100

    fix(SFT-2253): moved class references inside the conditionals and dynamically check for class existence before using them

--- packages/plugin/src/Bundles/Integrations/Plugins/Blitz/CalendarEventsBundle.php
+++ packages/plugin/src/Bundles/Integrations/Plugins/Blitz/CalendarEventsBundle.php
@@ -2,8 +2,6 @@
 
 namespace Solspace\Calendar\Bundles\Integrations\Plugins\Blitz;
 
-use putyourlightson\blitz\Blitz;
-use putyourlightson\blitz\drivers\purgers\DummyPurger;
 use Solspace\Calendar\Elements\Event as CalendarEvent;
 use Solspace\Calendar\Library\Bundles\BundleInterface;
 use yii\base\Event;
@@ -14,9 +12,19 @@ class CalendarEventsBundle implements BundleInterface
     public function __construct()
     {
         $plugins = \Craft::$app->getPlugins();
-        if ($plugins->isPluginInstalled('blitz') && $plugins->isPluginEnabled('blitz')) {
-            $purger = Blitz::$plugin->cachePurger;
-            if (!$purger instanceof DummyPurger) {
+
+        $blitzClass = 'putyourlightson\blitz\Blitz';
+        $dummyPurgerClass = 'putyourlightson\blitz\drivers\purgers\DummyPurger';
+
+        if (
+            $plugins->isPluginInstalled('blitz')
+            && $plugins->isPluginEnabled('blitz')
+            && class_exists($blitzClass)
+            && class_exists($dummyPurgerClass)
+        ) {
+            $purger = $blitzClass::$plugin->cachePurger;
+
+            if (!$purger instanceof $dummyPurgerClass) {
                 Event::on(
                     CalendarEvent::class,
                     CalendarEvent::EVENT_AFTER_SAVE,
