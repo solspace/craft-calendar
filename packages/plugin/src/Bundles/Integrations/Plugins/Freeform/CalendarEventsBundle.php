d3d07790ebe17ab72a6fdb6900e53ac158ec8adf
Sean Delaney <sean@solspace.com>
2025-07-25 14:54:54 +0100

    fix(SFT-2253): moved class references inside the conditionals and dynamically check for class existence before using them (#402)

--- packages/plugin/src/Bundles/Integrations/Plugins/Freeform/CalendarEventsBundle.php
+++ packages/plugin/src/Bundles/Integrations/Plugins/Freeform/CalendarEventsBundle.php
@@ -4,8 +4,6 @@ namespace Solspace\Calendar\Bundles\Integrations\Plugins\Freeform;
 
 use Solspace\Calendar\Integrations\Plugins\Freeform\CalendarEvents\CalendarEvents;
 use Solspace\Calendar\Library\Bundles\BundleInterface;
-use Solspace\Freeform\Events\Integrations\RegisterIntegrationTypesEvent;
-use Solspace\Freeform\Services\Integrations\IntegrationsService;
 use yii\base\Event;
 
 class CalendarEventsBundle implements BundleInterface
@@ -14,11 +12,22 @@ class CalendarEventsBundle implements BundleInterface
     {
         $plugins = \Craft::$app->getPlugins();
         $freeform = $plugins->getStoredPluginInfo('freeform');
-        if ($plugins->isPluginInstalled('freeform') && $plugins->isPluginEnabled('freeform') && $freeform && $freeform['version'] >= '5.0.0') {
+
+        $integrationsServiceClass = 'Solspace\Freeform\Services\Integrations\IntegrationsService';
+        $registerIntegrationTypesEventClass = 'Solspace\Freeform\Events\Integrations\RegisterIntegrationTypesEvent';
+
+        if (
+            $plugins->isPluginInstalled('freeform')
+            && $plugins->isPluginEnabled('freeform')
+            && class_exists($integrationsServiceClass)
+            && class_exists($registerIntegrationTypesEventClass)
+            && $freeform
+            && $freeform['version'] >= '5.0.0'
+        ) {
             Event::on(
-                IntegrationsService::class,
-                IntegrationsService::EVENT_REGISTER_INTEGRATION_TYPES,
-                function (RegisterIntegrationTypesEvent $event) {
+                $integrationsServiceClass,
+                \constant("{$integrationsServiceClass}::EVENT_REGISTER_INTEGRATION_TYPES"),
+                function ($event) {
                     $event->addType(CalendarEvents::class);
                 }
             );
